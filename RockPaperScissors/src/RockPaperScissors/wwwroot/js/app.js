8b823b163e7697cf206fc868fda3133b614df539
Donna Ryan <donna@awdd.net>
2016-10-22 17:15:15 -0700

    Bulk of the work is done. Get the the final results printed out and then move it all to the DOM and make 'er purdy-like.

--- RockPaperScissors/src/RockPaperScissors/wwwroot/js/app.js
+++ RockPaperScissors/src/RockPaperScissors/wwwroot/js/app.js
@@ -1,22 +1,56 @@
-// Board Pieces
-var pieces = ["rock", "paper", "scissors"];
-var txtRockWins = "Rock crushes scissors.";
-var txtPaperWins = "Paper covers rock.";
-var txtScissorsWins = "Scissors cuts paper.";
-// Computer's Play Setup
-function randomMove() {
-    var play = pieces[Math.floor(Math.random() * pieces.length)];
-    return play;
+var Player = (function () {
+    function Player(name) {
+        this.name = name;
+    }
+    Player.prototype.randomMove = function () {
+        //this.play = gameMoves[Math.floor(Math.random() * gameMoves.length)];
+        this.playID = Math.floor(Math.random() * gameMoves.length);
+    };
+    return Player;
+}());
+var Throw = (function () {
+    function Throw(name, beats, beatHow) {
+        this.name = name;
+        this.beats = beats;
+        this.beatHow = beatHow;
+        this.domID = this.name.toLowerCase();
+    }
+    return Throw;
+}());
+var throwRock = new Throw("Rock", "Scissors", "crushes");
+var selectRock = document.getElementById(throwRock.domID);
+var throwPaper = new Throw("Paper", "Rock", "covers");
+var selectPaper = document.getElementById(throwPaper.domID);
+var throwScissors = new Throw("Scissors", "Paper", "cut");
+var selectScissors = document.getElementById(throwScissors.domID);
+var gameMoves = [throwRock, throwPaper, throwScissors];
+var playerHuman = new Player("Human");
+var playerComputer = new Player("Computer");
+function evaluatePlay(humanPlay) {
+    playerComputer.randomMove();
+    var txtResults = "";
+    for (var key in gameMoves) {
+        if (gameMoves[key].name.toLowerCase() == humanPlay) {
+            playerHuman.playID = key;
+        }
+    }
+    console.log(playerHuman);
+    /*
+    txtResults = `You threw ${gameMoves[key].name}. The computer threw ${gameMoves[playerComputer.playID].name}. (${key}) `;
+    // which throw wins ?
+    if (gameMoves[key].beats === gameMoves[playerComputer.playID].name) {
+        txtResults += `${gameMoves[key].name} ${gameMoves[key].beatHow} ${gameMoves[playerComputer.playID].name}, so you win! (${key}) `;
+    }
+    else if (gameMoves[playerComputer.playID].beats === gameMoves[key].name) {
+        txtResults += `${gameMoves[playerComputer.playID].name} ${gameMoves[key].beatHow} ${gameMoves[key].beats}, so the computer wins! (${key}) `;
+    }
+    else if (gameMoves[key].name == gameMoves[playerComputer.playID].name) {
+        txtResults += ` You both threw the same, so it is a tied game! (${key}) `;
+    }
+
+    txtResults += `Care to try again? `;
+    */
 }
-// Grab the Player's Move
-var btnPaper = document.getElementById('paper');
-var btnRock = document.getElementById('rock');
-var btnScissors = document.getElementById('scissors');
-function savePlayersMove(player, computer) {
-    // highlight the player's selection
-    console.log("You chose " + player + ". The computer chose " + computer + ".");
-}
-var playersMove1 = btnPaper.addEventListener("click", function () { savePlayersMove("paper", randomMove()); });
-var playersMove2 = btnRock.addEventListener("click", function () { savePlayersMove("rock", randomMove()); });
-var playersMove3 = btnScissors.addEventListener("click", function () { savePlayersMove("scissors", randomMove()); });
-// Compare Computer to Player
+selectRock.addEventListener("click", function () { evaluatePlay("rock"); });
+selectPaper.addEventListener("click", function () { evaluatePlay("paper"); });
+selectScissors.addEventListener("click", function () { evaluatePlay("scissors"); });
--- RockPaperScissors/src/RockPaperScissors/wwwroot/js/app.ts
+++ RockPaperScissors/src/RockPaperScissors/wwwroot/js/app.ts
@@ -1,28 +1,63 @@
-﻿// Board Pieces
-let pieces = [`rock`, `paper`, `scissors`];
-let txtRockWins = `Rock crushes scissors.`;
-let txtPaperWins = `Paper covers rock.`;
-let txtScissorsWins = `Scissors cuts paper.`;
+﻿class Player {
+    public playID: number;                // not in the constructor because we don't want it on instantiation
+    constructor(public name: string) { }
 
-// Computer's Play Setup
-function randomMove() {
-    let play = pieces[Math.floor(Math.random() * pieces.length)];
-    return play;
+    randomMove() {
+        //this.play = gameMoves[Math.floor(Math.random() * gameMoves.length)];
+        this.playID = Math.floor(Math.random() * gameMoves.length);
+    }
+
 }
 
-// Grab the Player's Move
-let btnPaper = <HTMLImageElement>document.getElementById('paper');
-let btnRock = <HTMLImageElement>document.getElementById('rock');
-let btnScissors = <HTMLImageElement>document.getElementById('scissors');
-
-function savePlayersMove(player, computer) {
-    // highlight the player's selection
-    console.log(`You chose ${player}. The computer chose ${computer}.`);
+class Throw {
+    public domID: string;
+    constructor(public name: string, public beats: string, public beatHow: string) {
+        this.domID = this.name.toLowerCase();
+    }
+    
 }
 
-let playersMove1 = btnPaper.addEventListener(`click`, () => { savePlayersMove(`paper`, randomMove()); });
-let playersMove2 = btnRock.addEventListener(`click`, () => { savePlayersMove(`rock`, randomMove()); });
-let playersMove3 = btnScissors.addEventListener(`click`, () => { savePlayersMove(`scissors`, randomMove()); });
+let throwRock = new Throw(`Rock`, `Scissors`, `crushes`);
+let selectRock = <HTMLImageElement>document.getElementById(throwRock.domID);
+let throwPaper = new Throw(`Paper`, `Rock`, `covers`);
+let selectPaper = <HTMLImageElement>document.getElementById(throwPaper.domID);
+let throwScissors = new Throw(`Scissors`, `Paper`, `cut`);
+let selectScissors = <HTMLImageElement>document.getElementById(throwScissors.domID);
+
+let gameMoves = [throwRock, throwPaper, throwScissors];
+
+let playerHuman = new Player(`Human`);
+let playerComputer = new Player(`Computer`);
 
 
-// Compare Computer to Player
+function evaluatePlay(humanPlay) {
+    playerComputer.randomMove();
+    let txtResults = ``;
+    for (let key in gameMoves) {
+        if (gameMoves[key].name.toLowerCase() == humanPlay) {
+            playerHuman.playID = key;
+        }
+    }
+    console.log(playerHuman);
+    /*
+    txtResults = `You threw ${gameMoves[key].name}. The computer threw ${gameMoves[playerComputer.playID].name}. (${key}) `;
+    // which throw wins ?
+    if (gameMoves[key].beats === gameMoves[playerComputer.playID].name) {
+        txtResults += `${gameMoves[key].name} ${gameMoves[key].beatHow} ${gameMoves[playerComputer.playID].name}, so you win! (${key}) `;
+    }
+    else if (gameMoves[playerComputer.playID].beats === gameMoves[key].name) {
+        txtResults += `${gameMoves[playerComputer.playID].name} ${gameMoves[key].beatHow} ${gameMoves[key].beats}, so the computer wins! (${key}) `;
+    }
+    else if (gameMoves[key].name == gameMoves[playerComputer.playID].name) {
+        txtResults += ` You both threw the same, so it is a tied game! (${key}) `;
+    }
+
+    txtResults += `Care to try again? `;
+    */
+    
+}
+
+selectRock.addEventListener(`click`, () => { evaluatePlay(`rock`); });
+selectPaper.addEventListener(`click`, () => { evaluatePlay(`paper`); });
+selectScissors.addEventListener(`click`, () => { evaluatePlay(`scissors`); });
+
